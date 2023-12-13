2c1d17ee0ac4d524370a492e6e7f9aab48fc1578
drei <andredbenedetti@gmail.com>
2023-12-13 15:35:39 -0300

    log what port the server is listening to

--- server/index.ts
+++ server/index.ts
@@ -38,4 +38,6 @@ wss.on("connection", function connection(ws) {
   }, 1000 / messagesPerSecond)
 });
 
-server.listen(getPort())
+server.listen(getPort(), () => {
+  console.log(`Listening on port ${getPort()}`)
+})
